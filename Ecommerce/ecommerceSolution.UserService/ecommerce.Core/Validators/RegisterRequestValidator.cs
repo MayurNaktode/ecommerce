143a559787cb0a701aa2f91c35bd5f53ae397364
MAYUR\MAYUR <naktodemayur@gmail.com>
2025-07-16 21:59:12 +0530

    added fluent validation for registerrequest model

---
+++ Ecommerce/ecommerceSolution.UserService/ecommerce.Core/Validators/RegisterRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using ecommerce.Core.DTO;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecommerce.Core.Validators
+{
+    public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+    {
+        public RegisterRequestValidator()
+        {
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email Id is required").EmailAddress().WithMessage("Email Id is invalid");
+
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required");
+
+            RuleFor(x => x.PersonName).NotEmpty().WithMessage("PersonName is required").Length(1, 50).WithMessage("Person Name should be 1 to 50 characters long");
+
+            RuleFor(x => x.Gender).IsInEnum().WithMessage("Gender is required");
+        }
+    }
+}
